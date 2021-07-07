f25ea7d6365bafc75bd90367637b2709c02f9bff
Mateusz Boczarski <matbocz@outlook.com>
2021-07-07 13:57:07 +0200

    Create Name form and add it to Home page

--- hello.py
+++ hello.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'TEST_SECRET_KEY'
+
 moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Send")
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash("You changed your name!")
+        session['name'] = form.name.data
+
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
