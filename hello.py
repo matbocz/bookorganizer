4b913b36656bf345a8b32394d3c75aa6dfb16668
Mateusz Boczarski <matbocz@outlook.com>
2021-07-09 16:31:19 +0200

    Configure Flask-SQLAlchemy

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
+import os
+
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'TEST_SECRET_KEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 moment = Moment(app)
 
