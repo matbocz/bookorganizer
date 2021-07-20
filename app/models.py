6514513515386c094c40c270abe168feebabdf30
Mateusz Boczarski <matbocz@outlook.com>
2021-07-20 11:39:12 +0200

    Create user_loader callback

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
+from . import login_manager
 
 
 class Role(db.Model):
@@ -46,3 +47,8 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
