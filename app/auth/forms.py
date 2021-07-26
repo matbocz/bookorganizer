8f6d06594516cde1634612c3a1328b3ff36c348d
Mateusz Boczarski <matbocz@outlook.com>
2021-07-26 17:16:10 +0200

    Create user registration system

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo, ValidationError
+
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +10,38 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Log in')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('E-mail',
+                        validators=[DataRequired(message='Email is required.'),
+                                    Length(min=3, max=64,
+                                           message='Email must be between 3 and 64 characters long.'),
+                                    Email(message='Email must be valid.')])
+    username = StringField('Username',
+                           validators=[DataRequired(message='Username is required.'),
+                                       Length(min=3, max=64,
+                                              message='Username must be between 3 and 64 characters long.'),
+                                       Regexp(regex='^[A-Za-z][A-Za-z0-9_]*$',
+                                              flags=0,
+                                              message='Username must start with a letter '
+                                                      'and can only contain letters, numbers and underscores.')])
+
+    password = PasswordField('Password',
+                             validators=[DataRequired(message='Password is required.'),
+                                         Length(min=10, max=64,
+                                                message='Password must be between 10 and 64 characters long.')])
+    password2 = PasswordField('Confirm password',
+                              validators=[DataRequired(message='Password confirmation is required.'),
+                                          EqualTo(fieldname='password',
+                                                  message='Passwords must be identical.')])
+
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('This e-mail is already in use.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('This username is already in use.')
--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,8 @@ from flask import render_template, request, url_for, redirect, flash
 from flask_login import login_user, login_required, logout_user
 
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .. import db
 from ..models import User
 
 
@@ -27,3 +28,20 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data.lower(),
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('You can log in now.')
+
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html', form=form)
