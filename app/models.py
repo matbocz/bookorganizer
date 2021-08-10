3fa4f3527efede51b15918488e928fd39e33a479
Mateusz Boczarski <matbocz@outlook.com>
2021-08-10 14:24:20 +0200

    Add Default and Permissions columns to Role model

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class Role(db.Model):
     # Columns
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
 
     # Relationships
     users = db.relationship('User', backref='role', lazy='dynamic')
