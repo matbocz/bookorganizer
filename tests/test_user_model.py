2555a7007840055951a31a6d220c2063a646566e
Mateusz Boczarski <matbocz@outlook.com>
2021-08-11 15:53:23 +0200

    Add new tests to UserModelTestCase class

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,9 +1,22 @@
 import unittest
 
-from app.models import User
+from app import create_app, db
+from app.models import User, Role, Permission, AnonymousUser
 
 
 class UserModelTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
     def test_password_setter(self):
         u = User(password='book')
         self.assertTrue(u.password_hash is not None)
@@ -22,3 +35,19 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='book')
         u2 = User(password='book')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_user_role(self):
+        u = User(email='user@email.com', password='book')
+        self.assertTrue(u.can(Permission.Organize))
+        self.assertFalse(u.can(Permission.Admin))
+
+    def test_administrator_role(self):
+        r = Role.query.filter_by(name='Administrator').first()
+        u = User(email='user@email.com', password='book', role=r)
+        self.assertTrue(u.can(Permission.Organize))
+        self.assertTrue(u.can(Permission.Admin))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.Organize))
+        self.assertFalse(u.can(Permission.Admin))
