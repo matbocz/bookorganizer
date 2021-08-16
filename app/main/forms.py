fa8b3c7dcf201fcdc7b5ab34951bec178e10c674
Mateusz Boczarski <matbocz@outlook.com>
2021-08-16 15:22:27 +0200

    Create Edit Profile page

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,4 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import Length
 
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Name',
+                       validators=[Length(min=0, max=64,
+                                          message='Name must be between 0 and 64 characters long.')])
+    location = StringField('Location',
+                           validators=[Length(min=0, max=64,
+                                              message='Location must be between 0 and 64 characters long.')])
+    about_me = TextAreaField('About me')
+
+    submit = SubmitField('Save changes')
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template, flash, url_for
+from flask_login import login_required, current_user
+from werkzeug.utils import redirect
 
 from . import main
+from .forms import EditProfileForm
+from .. import db
 from ..models import User
 
 
@@ -14,3 +18,32 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
 
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        # Update current user data
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+
+        # Send current user data to database
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+
+        # Show message on page
+        flash('Your profile has been updated.')
+
+        # Redirect to current user page
+        return redirect(url_for('.user', username=current_user.username))
+
+    # Fill out form with current user data
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+
+    # Render Edit Profile page
+    return render_template('edit_profile.html', form=form)
