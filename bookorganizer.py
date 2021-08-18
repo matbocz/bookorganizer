aaa4914971b3c0f5ee804ed64c21ef258b8677b4
Mateusz Boczarski <matbocz@outlook.com>
2021-08-18 11:23:04 +0200

    Add Book model to shell context processor

--- bookorganizer.py
+++ bookorganizer.py
@@ -4,7 +4,7 @@ import click
 from flask_migrate import Migrate
 
 from app import create_app, db
-from app.models import User, Role, Permission
+from app.models import User, Role, Permission, Book
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -12,7 +12,7 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role, Permission=Permission)
+    return dict(db=db, Book=Book, User=User, Role=Role, Permission=Permission)
 
 
 @app.cli.command()
