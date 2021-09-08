ea2441bf768c9b1872833601b0ec0447472eade8
Mateusz Boczarski <matbocz@outlook.com>
2021-09-08 17:31:39 +0200

    Add function to create fake books

--- app/fake.py
+++ app/fake.py
@@ -1,8 +1,10 @@
+from random import randint
+
 from faker import Faker
 from sqlalchemy.exc import IntegrityError
 
 from app import db
-from app.models import User
+from app.models import User, Book
 
 
 def users(count=10):
@@ -24,3 +26,22 @@ def users(count=10):
             i += 1
         except IntegrityError:
             db.session.rollback()
+
+
+def books(count=10):
+    fake = Faker()
+
+    user_count = User.query.count()
+    for i in range(count):
+        date = fake.past_date()
+        u = User.query.offset(randint(0, user_count - 1)).first()
+
+        b = Book(title=fake.word(),
+                 author=fake.name(),
+                 description=fake.text(),
+                 date_added=date,
+                 date_modified=date,
+                 owner=u)
+        db.session.add(b)
+
+        db.session.commit()
