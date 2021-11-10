bf5c38d4c71a001d6d2a781342bf9ccf7c685e9e
Mateusz Boczarski <matbocz@outlook.com>
2021-11-10 12:39:48 +0100

    Add confirmed column to User model

--- app/models.py
+++ app/models.py
@@ -204,6 +204,7 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    confirmed = db.Column(db.Boolean, default=False)
 
     # Relationships
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
